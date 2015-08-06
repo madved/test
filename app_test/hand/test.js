cbcb4286c907862060dd2d8d9a03d0b2dd45dc86
Mikhail Bezzubikov <mikhail-sb@blend4web.com>
2015-08-06 18:36:40 +0300

    fixed app compile b4w.full.min.js

--- app_test/hand/test.js
+++ app_test/hand/test.js
@@ -28,12 +28,20 @@ function loaded_cb() {
 
 function render () {
   var now = new Date();
+  var hourHand   = scene.get_object_by_name("hour_hand");
+  var minuteHand = scene.get_object_by_name("minute_hand");
 	var secondHand = scene.get_object_by_name("second_hand");
-  var minutes = (6 * now.getMinutes()) * (Math.PI / 180);
-	var sec = (6 * now.getSeconds()) * (Math.PI / 180);
-  var milisec =(6 * now.getMilliseconds()) * (Math.PI / 180);
-  var secondAngle = (sec * -1) + (milisec / -1000);
-  var minuteAngle = (minutes * -1) + (milisec / -60000);
-    transform.set_rotation_euler(secondHand, 0, 0, secondAngle)
-console.log();
+
+  var hours    = (15 * now.getHours()) * (Math.PI / 90);
+  var minutes  = (6 * now.getMinutes()) * (Math.PI / 180);
+	var seconds  = (6 * now.getSeconds()) * (Math.PI / 180);
+  var milisec  = (6 * now.getMilliseconds()) * (Math.PI / 180);
+  var hourAngle   = (hours * 1) + (minutes / 60) + (seconds / 60 / 60);
+  var minuteAngle = (minutes * 1) + (seconds / 60) + (milisec / 60000);
+  var secondAngle = (seconds * 1) + (milisec / 1000);
+    transform.set_rotation_euler(hourHand, 0, 0, hourAngle);
+    transform.set_rotation_euler(minuteHand, 0, 0, minuteAngle);
+    transform.set_rotation_euler(secondHand, 0, 0, secondAngle);
+
+console.log(hourAngle);
 }
